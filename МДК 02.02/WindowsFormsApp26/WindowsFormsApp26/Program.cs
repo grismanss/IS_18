060c31253e28b75bfcf7aa4d2384b69aa3d19ba0
myName <s.s.grisman@mail.ru>
2020-09-28 09:20:17 +0700

    28/09/2020

---
+++ МДК 02.02/WindowsFormsApp26/WindowsFormsApp26/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp26
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        /// 
+
+       // создаем глобальную переменную для строки подключения
+        public static string connect_string = @"Data Source=505-S-S\SQLEXPRESS;Initial Catalog=restoran;Integrated Security=True";
+
+        
+      //  public static string connect_string2 = "Data Source=505-S-S\\SQLEXPRESS;Initial Catalog=restoran;Integrated Security=True";
+
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
